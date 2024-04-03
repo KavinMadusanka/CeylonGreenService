ec52a56a7577b60120ded8e0e12e2038dffca525
KavinMadusanka <kavinmadusanka20011@gmail.com>
2024-04-03 10:26:16 +0530

    new changes

--- Frontend/interface/src/pages/KAddcard.js
+++ Frontend/interface/src/pages/KAddcard.js
@@ -2,10 +2,17 @@ import React from 'react'
 import Layout2 from '../components/Layout/Layout2';
 import { FaCreditCard } from "react-icons/fa6";
 import { IoShieldCheckmark } from "react-icons/io5";
+import {}  from '../components/KAddcard.css'
+import KAddcard from './KAddcard';
 
 const KAddcard = () => {
   return (
     <Layout2>
+<<<<<<< Updated upstream
+=======
+        <div>
+            Card Details
+>>>>>>> Stashed changes
         <div className='KAboarder'>
             <h1 className='text-center'>Provide further information</h1>
             <div className='KApayment'><IoShieldCheckmark/> Your payment information is safe with us</div>
@@ -19,8 +26,17 @@ const KAddcard = () => {
                   </li>
                 </ul>
             </div>
+<<<<<<< Updated upstream
             <div>     
             </div>
+=======
+            <div>
+              
+            </div>
+            <div>     
+            </div>
+        </div>
+>>>>>>> Stashed changes
         </div>
     </Layout2>
   )
