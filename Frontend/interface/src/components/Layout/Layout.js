420542d1210f4756b0d76eec3d66b9797680f083
KavinMadusanka <kavinmadusanka20011@gmail.com>
2024-04-13 10:04:08 +0530

    delivery address frontend done

--- Frontend/interface/src/components/Layout/Layout.js
+++ Frontend/interface/src/components/Layout/Layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './Header';
 import Footer from './Footer';
 import {Helmet} from "react-helmet";
-import { ToastContainer } from 'react-toastify';
+import { Toaster } from "react-hot-toast";
 import 'react-toastify/dist/ReactToastify.css';
 const Layout = ({children, title, description, keywords, author }) => {
   return (
@@ -16,7 +16,7 @@ const Layout = ({children, title, description, keywords, author }) => {
         </Helmet>
         <Header/>
         <main style= {{minHeight:"65vh"}}>
-          <ToastContainer />
+          <Toaster />
           {children}
         </main>
         <Footer/>
--- Frontend/interface/src/components/Layout/Layout1.js
+++ Frontend/interface/src/components/Layout/Layout1.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Header1 from './Header1';
 import Footer from './Footer';
 import {Helmet} from "react-helmet";
-import { ToastContainer } from 'react-toastify';
+import { Toaster } from "react-hot-toast";
 import 'react-toastify/dist/ReactToastify.css';
 const Layout1 = ({children, title, description, keywords, author }) => {
   return (
@@ -16,7 +16,7 @@ const Layout1 = ({children, title, description, keywords, author }) => {
         </Helmet>
         <Header1/>
         <main style= {{minHeight:"65vh"}}>
-          <ToastContainer />
+          <Toaster />
           {children}
         </main>
         <Footer/>
