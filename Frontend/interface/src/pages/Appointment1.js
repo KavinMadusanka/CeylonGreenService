189553f997855732be1ee84409440f9c3ac5ff83
Navishka Gunawardana <128272519+it22570444@users.noreply.github.com>
2024-04-18 13:01:59 +0530

    appoint new

--- Frontend/interface/src/pages/Appointment1.js
+++ Frontend/interface/src/pages/Appointment1.js
@@ -1,28 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout1 from '../components/Layout/Layout1';
-import '../components/Appointment.css'
+import '../components/Appointment.css';
+import {toast} from 'react-toastify';
 
 const Appointment1 = () => {
+  const [fullName,setFullName] = useState("");
+  const [address,setAddress] = useState("");
+  const [phoneNumber,setPhoneNumber] = useState("");
+  const [email,setEmail] = useState("");
+  const [,] = useState("");
+  const [comments,setComments] = useState("");
+
+  // form function
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(fullName,address,phoneNumber,email,comments);
+    toast.success('Sent Appointment Successfully');
+  };
+
   return (
-    <Layout1>
+    <Layout1 title={'Ceylon Green - Appointment'}>
       <div className='border'>
         <form className="appointment-form">
       <h2>Appointment Form</h2>
       <div className="form-group">
         <label htmlFor="fullName">Full Name:</label>
-        <input type="text" id="fullName" name="fullName" placeholder="Enter your full name" />
+        <input 
+        type="text" 
+        value={fullName}
+        onChange={(e) => setFullName(e.target.value)}
+        placeholder="Enter your full name"
+        required />
       </div>
       <div className="form-group">
         <label htmlFor="address">Address:</label>
-        <textarea id="address" name="address" placeholder="Enter your address"></textarea>
+        <textarea 
+        value={address}
+        onChange={(e) => setAddress(e.target.value)}
+        placeholder="Enter your address"
+        required></textarea>
       </div>
       <div className="form-group">
         <label htmlFor="phoneNumber">Phone Number:</label>
-        <input type="tel" id="phoneNumber" name="phoneNumber" placeholder="Enter your phone number" />
+        <input 
+        type="tel" 
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+        placeholder="Enter your phone number"
+        required />
       </div>
       <div className="form-group">
         <label htmlFor="email">E-mail Address:</label>
-        <input type="email" id="email" name="email" placeholder="Enter your email address" />
+        <input 
+        type="email" 
+        value={email} 
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Enter your email address" 
+        required />
       </div>
       <div className="form-group">
         <label htmlFor="servicePackage">Service Package:</label>
@@ -33,7 +67,10 @@ const Appointment1 = () => {
       </div>
       <div className="form-group">
         <label htmlFor="comments">Additional Comments:</label>
-        <textarea id="comments" name="comments" placeholder="Enter any additional comments"></textarea>
+        <textarea 
+        value={comments} 
+        onChange={(e) => setComments(e.target.value)}
+        placeholder="Enter any additional comments"></textarea>
       </div>
       <div className="form-buttons">
         <button type="button">Cancel</button>
