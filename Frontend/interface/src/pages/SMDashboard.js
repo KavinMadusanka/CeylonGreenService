ef990c71933f6e39a3d202ce900fbd81a999a280
KavinMadusanka <kavinmadusanka20011@gmail.com>
2024-05-09 18:46:04 +0530

    payment dashboard edit

--- Frontend/interface/src/pages/SMDashboard.js
+++ Frontend/interface/src/pages/SMDashboard.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import axios from "axios";
+import Layout1 from "../components/Layout/Layout1";
 
 const SMDashboard = () => {
   const navigate = useNavigate();
@@ -14,13 +15,14 @@ const SMDashboard = () => {
     });
   };
   return (
-    <div className="container-fluid">
+    <Layout1>
+    <div className="">
       <div className="row flex-nowrap">
-        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0" style={{backgroundColor:"#BFEA7C"}}>
           <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
             <Link
               to="/smdashboard"
-              className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none"
+              className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-decoration-none" style={{color:'#416D19'}}
             >
               <span className="fs-5 fw-bolder d-none d-sm-inline">
                 Staff Manager
@@ -33,7 +35,7 @@ const SMDashboard = () => {
               <li className="w-100">
                 <Link
                   to="/smdashboard/SMHome"
-                  className="nav-link text-white px-0 align-middle"
+                  className="nav-link px-0 align-middle" style={{color:'#416D19'}}
                 >
                   <i className="fs-4 bi-speedometer2 ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">SMDashboard</span>
@@ -42,7 +44,7 @@ const SMDashboard = () => {
               <li className="w-100">
                 <Link
                   to="/smdashboard/Employee"
-                  className="nav-link px-0 align-middle text-white"
+                  className="nav-link px-0 align-middle" style={{color:'#416D19'}}
                 >
                   <i className="fs-4 bi-speedometer2 ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">
@@ -53,7 +55,7 @@ const SMDashboard = () => {
               <li className="w-100">
                 <Link
                   to="/smdashboard/Category"
-                  className="nav-link px-0 align-middle text-white"
+                  className="nav-link px-0 align-middle" style={{color:'#416D19'}}
                 >
                   <i className="fs-4 bi-columns ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">Category</span>
@@ -62,7 +64,7 @@ const SMDashboard = () => {
               <li className="w-100">
                 <Link
                   to="/smdashboard/EmployeeProfile"
-                  className="nav-link px-0 align-middle text-white"
+                  className="nav-link px-0 align-middle" style={{color:'#416D19'}}
                 >
                   <i className="fs-4 bi-person ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">Profile</span>
@@ -71,7 +73,7 @@ const SMDashboard = () => {
               <li className="w-100" onClick={handleLogout}>
                 <Link
                   to="http://localhost:3000/userLogin"
-                  className="nav-link px-0 align-middle text-white"
+                  className="nav-link px-0 align-middle" style={{color:'#416D19'}}
                 >
                   <i className="fs-4 bi-power ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">Logout</span>
@@ -81,7 +83,7 @@ const SMDashboard = () => {
           </div>
         </div>
         <div className="col p-0 m-0">
-          <div className="p-2 d-flex justify-content-center shadow">
+          <div className="p-2 d-flex justify-content-center">
             <h4>Employee Management System</h4>
           </div>
           <Outlet />
@@ -89,6 +91,7 @@ const SMDashboard = () => {
       </div>
       -
     </div>
+    </Layout1>
   );
 };
 
