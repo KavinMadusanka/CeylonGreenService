b3de880f3ec21b9412627b8cd95dd1faddd66938
KavinMadusanka <kavinmadusanka20011@gmail.com>
2024-05-11 11:29:44 +0530

    after payment done cart clear part is done

--- Frontend/routes/PaymentRoutes.js
+++ Frontend/routes/PaymentRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import {paymentController,
-    bankpaymentController
+    bankpaymentController,
+    updateProductQuantityController
 } from '../controllers/PaymentController.js'
+import { deleteAllCartItem } from '../controllers/CartController.js';
 
 //router object
 const router = express.Router();
@@ -9,5 +11,12 @@ const router = express.Router();
 //Post method
 router.post("/KAcardpayment", paymentController);
 router.post("/KApaymentForm", bankpaymentController);
+
+//delete part
+router.delete("/clear-cart/:email",deleteAllCartItem);
+
+//update quantity
+router.patch("/update-quantity/:productId", updateProductQuantityController);
+
 
 export default router;
